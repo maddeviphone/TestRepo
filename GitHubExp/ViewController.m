212d6fae1847f1715e7e4608048fb8dc51f3de79
madiphonedev <mad.iphonedev@gmail.com>
2013-10-21 23:19:41 +0530

    Put a log message in appDidFinishLaunching and added a button whose selector generate a simple log.

--- GitHubExp/ViewController.m
+++ GitHubExp/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-
+#define BUTTON_WIDTH     50.0
+#define BUTTON_HEIGHT    30.0
 @interface ViewController ()
 
 @end
@@ -20,10 +21,23 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)viewWillAppear:(BOOL)animated
+{
+    _button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    _button.frame = CGRectMake((self.view.frame.size.width - BUTTON_WIDTH)/2, (self.view.frame.size.height - BUTTON_HEIGHT)/2, BUTTON_WIDTH, BUTTON_HEIGHT);
+    [_button addTarget:self action:@selector(buttonClicked) forControlEvents:UIControlEventTouchDown];
+    [_button setTitle:@"Click" forState:UIControlStateNormal];
+    [self.view addSubview:_button];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)buttonClicked
+{
+    NSLog(@"Button Clicked");
 }
 
 @end
