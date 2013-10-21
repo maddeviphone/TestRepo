ec41ade3096c9150c705fc80c283cc4d8f42ba77
madiphonedev <mad.iphonedev@gmail.com>
2013-10-21 23:04:57 +0530

    Initial Commit

---
+++ GitHubExp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GitHubExp
+//
+//  Created by vikram.b.kumar on 10/21/13.
+//  Copyright (c) 2013 and. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GitHubExp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitHubExp
+//
+//  Created by vikram.b.kumar on 10/21/13.
+//  Copyright (c) 2013 and. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
