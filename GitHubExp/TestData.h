477831d030e0a015c3e775fc883900387f360235
madiphonedev <maddeviphone@gmail.com>
2013-10-23 23:54:36 +0530

    Vik:	Added File

---
+++ GitHubExp/TestData.h
@@ -0,0 +1,13 @@
+//
+//  TestData.h
+//  GitHubExp
+//
+//  Created by vikram.b.kumar on 10/23/13.
+//  Copyright (c) 2013 and. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestData : NSObject
+
+@end
---
+++ GitHubExp/TestData.m
@@ -0,0 +1,13 @@
+//
+//  TestData.m
+//  GitHubExp
+//
+//  Created by vikram.b.kumar on 10/23/13.
+//  Copyright (c) 2013 and. All rights reserved.
+//
+
+#import "TestData.h"
+
+@implementation TestData
+
+@end
